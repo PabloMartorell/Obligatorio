eb2433b2dcf1ca86d172a82adc3e3271a3061481
Francisco Cannepa <fran981942@hotmail.com>
2021-05-16 19:49:28 -0300

    master

---
+++ main.js
@@ -0,0 +1,34 @@
+const RegistrarUsuarios = () => {
+    
+
+}
+
+const LogearUsuario = () => {
+
+
+}
+
+<div class="Dashboard">
+            <h1>Panel Alumno</h1>
+            <br>
+            <div>
+
+                <div class="Gestion">
+                    <!-- <h1>Bienvenido NombreProf</h1> -->
+                    <br>
+                    <!-- Apartado Alumno -->
+
+                    <div class="AlumnoList">
+                        <select id="AlumnoDisplay">
+                            <option value="1">Alumno1</option>
+                            <option value="2">Alumno2</option>
+                        </select>
+                    </div>
+
+                    <div class="AlumnoInfo">
+                        <label>Alumno</label>
+                    </div>
+
+                </div>
+            </div>
+        </div>
